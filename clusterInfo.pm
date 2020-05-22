3316b1de0af4bf8f0497590db2c381b287ef26af
Brian Doyle <62153992+snapshot4@users.noreply.github.com>
2020-05-22 13:39:59 -0400

    Update clusterInfo.pm

--- clusterInfo.pm
+++ clusterInfo.pm
@@ -3,6 +3,8 @@
 #Author: Brian Doyle
 #Name: clusterList.pm
 #Description: This is meant to be a secured file containing the cluster information.
+#Instructions: Create a readonly user in the Cohesity UI and use that in the below configurations.  
+# Next run iris_cli customer_reporting db at the Cohesity command line, this will provide the database name.
 
 package clusterInfo;
 
