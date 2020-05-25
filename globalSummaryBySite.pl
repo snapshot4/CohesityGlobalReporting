ab15c67d4b119ca78c64bd44cf13cec67194fb26
Brian Doyle <62153992+snapshot4@users.noreply.github.com>
2020-05-25 12:03:37 -0400

    Add files via upload

--- globalSummaryBySite.pl
+++ globalSummaryBySite.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-our $version=1.0.2;
+our $version=1.0.3;
 
 # Author: Brian Doyle
 # Name: globalSummaryBySite.pl
@@ -8,6 +8,8 @@ our $version=1.0.2;
 # 1.0.0 - Added debug to assist if problems arise in script.
 # 1.0.0 - Added display variable to output to display as tab seperated or in HTML format.
 # 1.0.1 - Externalized the cluster list so to make this list more secure.
+# 1.0.2 - Return rows that even count to 0
+# 1.0.3 - Added in KBytes written
 
 # Modules
 use strict;
@@ -18,12 +20,18 @@ use Time::HiRes;
 use clusterInfo;
 
 # Global Variables
-my $display=0; #(0-Standard Display, 1-HTML)
+my $display=1; #(0-Standard Display, 1-HTML)
 my $debug=0; #(0-No log messages, 1-Info messages, 2-Debug messages)
 my $hoursAgo=24;
 my $title="Global Cohesity Report by Site";
+my $reportLabel="TB"; # Values(TB,GB,MB,KB)
 my %sites;
 my @clusters=clusterInfo::clusterList();
+my $reportValue;
+if ($reportLabel eq "TB") { $reportValue=1000*1000*1000; }
+if ($reportLabel eq "GB") { $reportValue=1000*1000; }
+if ($reportLabel eq "MB") { $reportValue=1000; }
+if ($reportLabel eq "KB") { $reportValue=1; }
 
 #Set Environment Variable to no verify certs
 $ENV{'PERL_LWP_SSL_VERIFY_HOSTNAME'} = 0;
@@ -68,9 +76,9 @@ sub getDbInfo {
 }
 
 sub printHeader {
-  printf "Cluster\t\tTotal\tSuccess\tFailed\tSuccess Rate\n" if ($display==0);
-  printf "<TABLE BORDER=1 ALIGN=center><TR BGCOLOR=lightgreen><TD ALIGN=center colspan='5'>$_[0] Report</TD></TR>" if ($display==1);
-  printf "<TR BGCOLOR=lightgreen><TD>Cohesity Cluster</TD><TD>Total</TD><TD>Success</TD><TD>Failed</TD><TD>Success Rate</TD></TR>" if ($display==1);
+  printf "Cluster\t\tTotal\tSuccess\tFailed\tSuccess Rate\tWritten($reportLabel)\n" if ($display==0);
+  printf "<TABLE BORDER=1 ALIGN=center><TR BGCOLOR=lightgreen><TD ALIGN=center colspan='6'>$_[0] Report</TD></TR>" if ($display==1);
+  printf "<TR BGCOLOR=lightgreen><TD>Cohesity Cluster</TD><TD>Total</TD><TD>Success</TD><TD>Failed</TD><TD>Success Rate</TD><TD>Written($reportLabel)</TD></TR>" if ($display==1);
 }
 
 sub dataGather {
@@ -82,7 +90,7 @@ sub dataGather {
   foreach my $href (@clusters){
     print "Connecting to Database $href->{'databaseName'}\n" if ($debug>=2);
     my $dbh = DBI -> connect("dbi:Pg:dbname=$href->{'databaseName'};host=$href->{'nodeIp'};port=$href->{'port'}",$href->{'defaultUsername'},$href->{'defaultPassword'}) or die $DBI::errstr;
-    my $sql = "SELECT SUM(total_num_entities),SUM(success_num_entities),SUM(failure_num_entities),cluster_name 
+    my $sql = "SELECT SUM(total_num_entities),SUM(success_num_entities),SUM(failure_num_entities),cluster_name,SUM(data_written_size_bytes) 
                FROM reporting.protection_job_runs,reporting.cluster 
                WHERE start_time_usecs >= $startTimeUsecs
                AND reporting.protection_job_runs.cluster_id = reporting.cluster.cluster_id
@@ -92,9 +100,20 @@ sub dataGather {
     my $sth = $dbh->prepare($sql);
     print "Executing Query\n" if ($debug>=2);
     $sth->execute() or die DBI::errstr;
-    while(my @row=$sth->fetchrow_array){
-      $sites{$row[3]}="$row[0],$row[1],$row[2]";
+    my @rows=$sth->fetchrow_array;
+    $sth->finish();
+    print "TEST: ".@rows.length."\n" if($debug>=2);
+    if(@rows.length == 0){
+      my $sth=$dbh->prepare("SELECT cluster_name FROM reporting.cluster");
+      $sth->execute();
+      my $cluster=$sth->fetch()->[0];
+      $sth->finish();
+      @rows=(0,0,0,"$cluster");
     }
+    print "TEST: ".@rows.length."\n" if($debug>=2);
+    #while(my @row=$sth->fetchrow_array){
+      $sites{$rows[3]}="$rows[0],$rows[1],$rows[2],$rows[4]";
+    #}
   }
 }
 
@@ -110,8 +129,9 @@ sub printReport {
     } else {
       $successRate=0;
     }
-    printf "$clusterName\t\t$cols[0]\t$cols[1]\t$cols[2]\t%2.1f%\n",$successRate if ($display==0);
-    printf "<TR><TD>$clusterName</TD><TD ALIGN=center>$cols[0]</TD><TD ALIGN=center>$cols[1]</TD><TD ALIGN=center>$cols[2]</TD><TD ALIGN=right>%2.1f%</TD></TR>",$successRate if ($display==1);
+    my $dataWritten=$cols[3]/$reportValue;
+    printf "$clusterName\t\t$cols[0]\t$cols[1]\t$cols[2]\t%2.1f%\t%2.1f\n",$successRate,$dataWritten if ($display==0);
+    printf "<TR><TD>$clusterName</TD><TD ALIGN=center>$cols[0]</TD><TD ALIGN=center>$cols[1]</TD><TD ALIGN=center>$cols[2]</TD><TD ALIGN=right>%2.1f%</TD><TD>%2.1f</TD></TR>",$successRate,$dataWritten if ($display==1);
   }
   printf "</TABLE></BODY></HTML>\n" if ($display==1);
 }
